8bc4efc5ded7aa38975809a182ad5bdd95cbd2f7
Niel Sean Mikhael Gildo <rhythmheavenr3@gmail.com>
2023-06-07 08:17:03 +0800

    Added Toastify Alerts

--- preload.js
+++ preload.js
@@ -1,5 +1,10 @@
 const { contextBridge, ipcRenderer } = require("electron");
+const Toastify = require('toastify-js');
 
 contextBridge.exposeInMainWorld("axios", {
     openAI: (code_line) => ipcRenderer.invoke('axios.openAI',code_line)
+});
+
+contextBridge.exposeInMainWorld("Toastify", {
+    showToast: (options) => Toastify(options).showToast()
 });
--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -5,9 +5,29 @@ if (form) {
 
     const formData = new FormData(form);
 
-    
+    let codeCheck = formData.get("code_line");
 
+
+    if(codeCheck <= 8){
+    alertMessage("error","Please input at least 8 characters");
+    return;
+  }
     const response = await window.axios.openAI(formData.get("code_line"));
     document.getElementById("run_result").innerHTML = JSON.stringify(response.choices[0].text);
   };
+}
+
+function alertMessage(status,codeCheck){
+  window.Toastify.showToast({
+    text: codeCheck,
+    duration: 5000,
+    stopOnFocus: true,
+    style: {
+      textAlign: "center",
+      background: status == "error" ? "red":"green",
+      color: "white",
+      padding: "5px",
+      marginTop: "2px"
+    }
+  });
 }
